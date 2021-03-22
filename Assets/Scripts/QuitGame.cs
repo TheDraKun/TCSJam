d86527053e3b46a0c26e6530d7e58d47f22caff2
Deepak Nelson <deepakkris123@gmail.com>
2021-03-22 22:21:15 +0530

    Minor Script Edits

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -8,6 +8,6 @@ public class QuitGame : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
-        Debug.Log("q");
+        
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,18 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] gameobjs;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spawn();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     public void spawn()
     {
         int randomIndex = Random.Range(0, gameobjs.Length);
